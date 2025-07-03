0bc56a4bf9b1ded0dc99886da297f201409de726
Kavith Lokuhewage <kaviththiranga@gmail.com>
2025-07-03 18:26:04 +0530

    Comment out origin checks in message event listener in region-select.js

--- region-select.js
+++ region-select.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
   window.addEventListener('message', (event) => {
-    if (event.origin === 'https://choreo.dev' || event.origin === 'http://localhost:3002') {
+    // if (event.origin === 'https://choreo.dev' || event.origin === 'http://localhost:3002') {
       if (event.data.type === 'checkRecentOrg') {
         const data = localStorage.getItem('recent-org');
         event.source.postMessage({ recentOrg: data }, event.origin);
       }
-    }
+    // }
   });
 });
